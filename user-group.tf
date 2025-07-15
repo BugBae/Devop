43cbb6a79611483edfa71aa50b14fadb6c308576
BugBae <rrajeshwari88398@gmail.com>
2025-07-15 10:06:59 +0530

    Update user-group.tf

--- user-group.tf
+++ user-group.tf
@@ -1,7 +1,7 @@
 
 
-resource "aws_iam_group_membership" "add_user_to_group" {
+resource "aws_iam_group_membership" "raje" {
   name  = "s3-access"
-  users = [aws_iam_user..name]
-  group = aws_iam_group.my_group.name
+  users = [aws_iam_user.qwerty.name]
+  group = aws_iam_group.raje.name
 }
